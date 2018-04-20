f4ba527faf14cae28995c1cc2484699525bdac8d
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2018-04-20 21:04:46 +0300

    Added update handler

--- mywebapp/profile.php
+++ mywebapp/profile.php
@@ -91,7 +91,7 @@ require_once ('application/models/User.php');
                     <p>
                 </form>
                 <div id="profileImage">
-                    <img src="uploads/<?= $userData['profile_avatar'] ?>">
+                    <img src="uploads/<?= $userData['profile_avatar'] ?>" width="300px" height="280px">
                 </div>
             </div>
         </div>
